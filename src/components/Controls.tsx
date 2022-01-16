4715b5b269ebeccfc44df4a551514e7e74bcef80
ewal <lofquist@gmail.com>
2022-01-16 16:11:54 +0100

    Fixes control states

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,11 +1,20 @@
 import "./Controls.css";
-import Game from "../logic/game";
+import Game, { GameEvent } from "../logic/game";
+import { useEffect, useState } from "react";
 
 const game = Game.getInstance();
 
 const Controls: React.FC = () => {
+  const [state, setState] = useState({
+    isRunning: false,
+    dimension: { x: game.gridDimension.x, y: game.gridDimension.y },
+    gameState: GameEvent.INIT,
+    visuals: 0,
+    generations: 0,
+  });
+
   const handleStartStop = () => {
-    if (game.isRunning) {
+    if (state.isRunning) {
       game.stop();
     } else {
       game.start();
@@ -24,23 +33,118 @@ const Controls: React.FC = () => {
     game.slower();
   };
 
+  const buttonText = () => {
+    if (state.isRunning === false && state.generations === 0) {
+      return "Start";
+    }
+
+    return state.isRunning ? "Pause" : "Continue";
+  };
+
+  const handleX = (e: React.FormEvent<HTMLInputElement>) => {
+    game.gridDimension = {
+      x: Number(e.currentTarget.value),
+      y: state.dimension.y,
+    };
+  };
+
+  const handleY = (e: React.FormEvent<HTMLInputElement>) => {
+    game.gridDimension = {
+      x: state.dimension.x,
+      y: Number(e.currentTarget.value),
+    };
+  };
+
+  const handleVisualChange = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log("E", e);
+    setState({ ...state, visuals: Number(e.currentTarget.value) });
+  };
+
+  const handleGameEvent = (instance: Game, message: GameEvent): void => {
+    if (
+      message === GameEvent.SETTING_CHANGE ||
+      message === GameEvent.INIT ||
+      message === GameEvent.START ||
+      message === GameEvent.STOP ||
+      message === GameEvent.RESET
+    ) {
+      setState({
+        isRunning: instance.isRunning,
+        dimension: { x: instance.gridDimension.x, y: instance.gridDimension.y },
+        gameState: message,
+        visuals: 0,
+        generations: instance.generations,
+      });
+    }
+  };
+
+  useEffect(() => {
+    game.onGameEvent.subscribe(handleGameEvent);
+
+    return () => game.onGameEvent.unsubscribe(handleGameEvent);
+  }, []);
+
   return (
-    <div className="controls">
-      <button type="button" onClick={handleStartStop}>
-        Start / pause
-      </button>
-      <button type="button" onClick={handleReset}>
-        Reset
-      </button>
-      <div className="group">
-        <button type="button" onClick={handleSlower}>
-          Slower
+    <form className="controls">
+      <fieldset className="dimensions">
+        <legend>Settings</legend>
+        <div className="con">
+          <label>
+            <span>X</span>
+            <input
+              onChange={handleX}
+              value={state?.dimension.x}
+              type="number"
+              step="5"
+              max="100"
+              min="10"
+              disabled={state.isRunning || state.generations > 0}
+            />
+          </label>
+          <label>
+            <span>Y</span>
+            <input
+              onChange={handleY}
+              value={state?.dimension.y}
+              type="number"
+              step="5"
+              max="100"
+              min="10"
+              disabled={state.isRunning || state.generations > 0}
+            />
+          </label>
+        </div>
+      </fieldset>
+      <div className="sep" />
+      <fieldset className="actions">
+        <legend>Controls</legend>
+        <button type="button" onClick={handleStartStop}>
+          {buttonText()}
         </button>
-        <button type="button" onClick={handleFaster}>
-          Faster
+        <div className="group">
+          <button type="button" onClick={handleSlower}>
+            Slower
+          </button>
+          <button type="button" onClick={handleFaster}>
+            Faster
+          </button>
+        </div>
+        <button type="button" onClick={handleReset} disabled={state.isRunning}>
+          Reset
         </button>
-      </div>
-    </div>
+      </fieldset>
+      <fieldset className="visuals">
+        <legend>Visuals</legend>
+        <input
+          type="range"
+          value={state.visuals}
+          onChange={handleVisualChange}
+          min="1"
+          max="100"
+          aria-label="Trace cells"
+        />
+      </fieldset>
+    </form>
   );
 };
 
