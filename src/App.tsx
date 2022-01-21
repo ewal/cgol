07587274594bf1b88beb8b1c2ea8bee5b9beef93
ewal <lofquist@gmail.com>
2022-01-21 19:32:08 +0100

    Add link to the github repo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Grid from "./components/Grid";
 import Controls from "./components/Controls";
 import Game from "./logic/game";
 import Stats from "./components/Stats";
+import GithubLogo from "./assets/img/GitHub-Mark-Light-64px.png";
 
 const game = Game.getInstance();
 
@@ -28,6 +29,19 @@ const App: React.FC = () => {
         <Stats />
         <div className="separator" />
         <Controls />
+        <code className="github-link">
+          <img
+            src={GithubLogo}
+            style={{ width: 16, height: 16 }}
+            alt="Github"
+          />
+          <a
+            href="https://github.com/ewal/cgol"
+            title="Github repository for the project"
+          >
+            ewal/cgol
+          </a>
+        </code>
       </aside>
       <main className="right">
         <Grid />
