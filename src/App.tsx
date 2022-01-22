d76a3f7b9cb9a886bcf706f4330f574a056c39c9
ewal <lofquist@gmail.com>
2022-01-22 15:41:43 +0100

    Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import "./App.css";
 
 import React, { useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import Grid from "./components/Grid";
 import Controls from "./components/Controls";
 import Game from "./logic/game";
 import Stats from "./components/Stats";
 import GithubLogo from "./assets/img/GitHub-Mark-Light-64px.png";
+
+const firebaseConfig =
+  process.env.NODE_ENV === "production"
+    ? {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
+      }
+    : {};
 
 const game = Game.getInstance();
 
@@ -19,6 +35,11 @@ const App: React.FC = () => {
       gridDimension: game.gridDimension,
       initialAlive,
     });
+
+    if (process.env.NODE_ENV === "production") {
+      const app = initializeApp(firebaseConfig);
+      getAnalytics(app);
+    }
   }, []);
 
   return (
